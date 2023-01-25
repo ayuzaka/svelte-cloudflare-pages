316f5a32832520b0173d774bed23b51400e5a374
ayuzaka <ayuzaka@vivaldi.net>
2023-01-25 22:26:36 +0900

    style: fix

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-  import SvelteMarkdown from 'svelte-markdown'
-	import type { PageData } from './$types'
+	import SvelteMarkdown from 'svelte-markdown';
+	import type { PageData } from './$types';
 
-	export let data: PageData
-
+	export let data: PageData;
 </script>
 
 <h1>README</h1>
 <SvelteMarkdown source={data.contents} />
 
-
 <style>
-h1 {
-	font-size: 2rem;
-}
+	h1 {
+		font-size: 2rem;
+	}
 </style>
