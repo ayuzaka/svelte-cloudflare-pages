ca00acf114ea889607803ac8e3a2b51b9058aef7
ayuzaka <ayuzaka@vivaldi.net>
2023-02-01 18:08:34 +0900

    feat: add portal component

---
+++ src/lib/components/Portal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+
+	let ref: HTMLElement;
+
+	onMount(() => {
+		document.body.appendChild(ref);
+	});
+
+	onDestroy(() => {
+		document.body.removeChild(ref);
+	});
+</script>
+
+<div bind:this={ref}>
+	<slot />
+</div>
---
+++ src/routes/portal/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Portal from '../../lib/components/Portal.svelte';
+
+	let displayPortal = false;
+
+	function togglePortal() {
+		displayPortal = !displayPortal;
+	}
+</script>
+
+<div>
+	<p>foo bar</p>
+	{#if displayPortal}
+		<Portal>Portal contents</Portal>
+	{/if}
+</div>
+
+<button type="button" on:click={togglePortal}> Toggle Portal </button>
